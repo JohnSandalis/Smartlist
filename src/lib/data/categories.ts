0b0db0429a7835fc9efedd89277d0dfc7903ab2f
Giannis Sandalis <72869500+JohnSandalis@users.noreply.github.com>
2025-04-12 12:55:06 +0000

    chore(categories): add local category images

--- src/lib/data/categories.ts
+++ src/lib/data/categories.ts
@@ -5,133 +5,114 @@ export const categories: Category[] = [
     name: "Fruits & Vegetables",
     greek_name: "Φρούτα & Λαχανικά",
     id: "fruits_and_vegetables",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000187?c=1640094003",
+    img_url: "/images/categories/fruits_and_vegetables.png",
   },
   {
     name: "Milk & Dairy Products",
     greek_name: "Γάλα & Γαλακτοκομικά Προϊόντα",
     id: "milk_and_dairy_products",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000190?c=1640093952",
+    img_url: "/images/categories/milk_and_dairy_products.png",
   },
   {
     name: "Coffee, Tea & Cocoa",
     greek_name: "Καφές, Τσάι & Κακάο",
     id: "coffee_tea_and_cocoa",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000128?c=1640097215",
+    img_url: "/images/categories/coffee_tea_and_cocoa.png",
   },
   {
     name: "Eggs & Ointments",
     greek_name: "Αυγά & Αλοιφές",
     id: "eggs_and_ointments",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000192?c=1640093919",
+    img_url: "/images/categories/eggs_and_ointments.png",
   },
   {
     name: "Meat & Fish",
     greek_name: "Κρέας & Ψάρι",
     id: "meat_and_fish",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000188?c=1640093980",
+    img_url: "/images/categories/meat_and_fish.png",
   },
   {
     name: "Refrigerated Products & Ice",
     greek_name: "Κατεψυγμένα Προϊόντα & Πάγος",
     id: "refrigerated_products_and_ice",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000196?c=1640093852",
+    img_url: "/images/categories/refrigerated_products_and_ice.png",
   },
   {
     name: "Beverages, Juices & Other Drinks",
     greek_name: "Αναψυκτικά, Χυμοί & Άλλα Ποτά",
     id: "beverages_juices_and_other_drinks",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000198?c=1640093820",
+    img_url: "/images/categories/beverages_juices_and_other_drinks.png",
   },
   {
     name: "Alcoholic Drinks",
     greek_name: "Αλκοολούχα Ποτά",
     id: "alcoholic_drinks",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000197?c=1640093831",
+    img_url: "/images/categories/alcoholic_drinks.png",
   },
   {
     name: "Cheese",
     greek_name: "Τυρί",
     id: "cheese",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000191?c=1640093931",
+    img_url: "/images/categories/cheese.png",
   },
   {
     name: "Cold Cuts & Delicatessen",
     greek_name: "Αλλαντικά",
     id: "cold_cuts_and_delicatessen",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000194?c=1640093902",
+    img_url: "/images/categories/cold_cuts_and_delicatessen.png",
   },
   {
     name: "Cereals, Snacks & Nuts",
     greek_name: "Δημητριακά, Σνακ & Ξηροί Καρποί",
     id: "cereals_snacks_and_nuts",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000199?c=1640093811",
+    img_url: "/images/categories/cereals_snacks_and_nuts.png",
   },
   {
     name: "Honey, Jam & Spreads",
     greek_name: "Μέλι, Μαρμελάδα & Spreads",
     id: "honey_jam_and_spreads",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000125?c=1640097173",
+    img_url: "/images/categories/honey_jam_and_spreads.png",
   },
   {
     name: "Ready-Made Meals",
     greek_name: "Έτοιμα Φαγητά",
     id: "ready_made_meals",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000195?c=1640093887",
+    img_url: "/images/categories/ready_made_meals.png",
   },
   {
     name: "Pasta & Rice",
     greek_name: "Ζυμαρικά & Ρύζι",
     id: "pasta_and_rice",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000186?c=1640094010",
+    img_url: "/images/categories/pasta_and_rice.png",
   },
   {
     name: "Grains",
     greek_name: "Σιτηρά",
     id: "grains",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000144?c=1640097830",
+    img_url: "/images/categories/grains.png",
   },
   {
     name: "Groceries",
     greek_name: "Παντοπωλείο",
     id: "groceries",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000203?c=1640093759",
+    img_url: "/images/categories/groceries.png",
   },
   {
     name: "Bread & Pastry",
     greek_name: "Ψωμί & Αρτοσκευάσματα",
     id: "bread_and_pastry",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000186?c=1640094010",
+    img_url: "/images/categories/bread_and_pastry.png",
   },
   {
     name: "Personal Hygiene",
     greek_name: "Προσωπική Υγιεινή",
     id: "personal_hygiene",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000205?c=1640093708",
+    img_url: "/images/categories/personal_hygiene.png",
   },
   {
     name: "Home Care",
     greek_name: "Φροντίδα Σπιτιού",
     id: "home_care",
-    img_url:
-      "https://cdn.e-food.gr/cdn-cgi/image/w=160,h=160,f=auto,dpr=2/restaurants/2639051/thumb_category/0000000000206?c=1640093696",
+    img_url: "/images/categories/home_care.png",
   },
 ];
